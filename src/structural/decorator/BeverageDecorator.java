904fe21d854e609ae4d8911b77ddc89ad2f31ab2
Ivan <Ivan@DESKTOP-TN6HJQT>
2018-07-16 22:18:12 +0200

    Decorator

--- src/structural/decorator/BeverageDecorator.java
+++ src/structural/decorator/BeverageDecorator.java
@@ -3,14 +3,6 @@ package structural.decorator;
 public abstract class BeverageDecorator extends Beverage{
 
 	public BeverageDecorator(Beverage beverage, String decoration, double price) {
-
-		super(beverage.getName(), beverage.getPrice());
-		super.setName(super.getName()+" + "+decoration);
-		super.setPrice(super.getPrice()+price);
-	}
-	
-	@Override
-	public void getBeverageInfo() {
-		System.out.println(super.getName()+": "+super.getPrice());
+		super(beverage.getName()+" + "+decoration, beverage.getPrice()+price);
 	}
 }
